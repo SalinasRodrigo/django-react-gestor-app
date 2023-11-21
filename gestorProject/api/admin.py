3d2564e7a44d29b19c27c38e216a0b22e3260059
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2023-11-21 19:38:46 -0300

    modelos de gastos e ingresos

--- gestorProject/api/admin.py
+++ gestorProject/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Gasto, Ingreso
 
+admin.site.register(Gasto)
+admin.site.register(Ingreso)
 # Register your models here.
--- gestorProject/api/models.py
+++ gestorProject/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Gasto (models.Model):
+  descripcion = models.CharField(max_length=250)
+  cantidad = models.IntegerField()
+  fecha = models.DateField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+class Ingreso (models.Model):
+  descripcion = models.CharField(max_length=250)
+  cantidad = models.IntegerField()
+  fecha = models.DateField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
