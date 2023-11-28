4a282a7f042fd3b9f6640f2157b5ffc74fa721af
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2023-11-27 23:09:51 -0300

    se agregaron inputs y diseños basicos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,26 @@ import {ingresos, gastos} from "./mooks/info.json"
 import { MyTable } from "./components/MyTable";
 import { MyChart } from "./components/MyChart";
 import { MyDonut } from "./components/MyDonut";
+import { useState } from "react";
 
 function App() {
+  const [ings, setIngresos] = useState(ingresos)
+  const [gast, setGastos] = useState(gastos)
   return (
     <>
       <main>
-        <MyTable head={['Fecha', 'Monto']} ings={ingresos} gast={gastos}/>
+        <MyTable head={['Fecha', 'Monto']} ings={ings} gast={gast}/>
         <Card className="dona1"><MyDonut /></Card>
-        <Card className="dona2">Dona 2</Card>
+        <Card className="formulario">
+          <form>
+            <label htmlFor="descripcion">Descripción</label>
+            <input className="my-input" type="text" name="descripcion"  />
+            <label htmlFor="monto">Monto</label>
+            <input className="my-input" type="number" name="monto" />
+            <label htmlFor="fecha">Fecha</label>
+            <input className="my-input" type="date" name="fecha" id="" />
+          </form>
+        </Card>
         <MyChart chartdata={gastos}/>
       </main>
     </>
