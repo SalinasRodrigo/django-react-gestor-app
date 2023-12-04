705602c4c8fb115a1fe1443ae62b8a02e35fd72d
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2023-12-03 22:18:52 -0300

    cambios a los settings para conectar con postgres

--- gestorProject/gestorProject/settings.py
+++ gestorProject/gestorProject/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'gestorProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "gestor_db",
+        "USER": "Rodrigo",
+        "PASSWORD": "0000",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
